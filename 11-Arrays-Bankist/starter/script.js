14a1fd49078c479e0ca30523f5992f5e80820181
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-07-16 12:24:04 -0400

    loops

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -71,7 +71,7 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
 
@@ -95,3 +95,25 @@ console.log([...arr, ...arr2])
 
 // JOIN
 console.log(letters.join(' - '))
+
+
+// FOR EACH LOOP OF ARRAYS
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+//for of loop
+// for (const movement of movements)
+    for (const [i, movement] of movements.entries()) { //allows you to access counter if you loop over movement.entries()
+  if (movement > 0){
+    console.log(`Movement ${i +1 }: you deposited ${movement}`)
+    } else {
+      console.log(`Movement ${i + 1}: you withdrew ${Math.abs(movement)}`)
+  }
+}
+console.log('------FOREACH------')
+//for each loop - needs a callback function (cannot break out of with break statement)
+movements.forEach(function(mov, i, arr) {
+  if (mov > 0){
+    console.log(`Movement ${i + 1 }: you deposited ${mov}`)
+    } else {
+      console.log(`Movement ${i + 1}: you withdrew ${Math.abs(mov)}`)
+  }
+})
