538a781f0eb4f9b519684bd5dd648c06ab8f2bca
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-07-16 12:28:52 -0400

    sets

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -65,11 +65,11 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // LECTURES
 
-const currencies = new Map([
-  ['USD', 'United States dollar'],
-  ['EUR', 'Euro'],
-  ['GBP', 'Pound sterling'],
-]);
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
 
 // const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
@@ -116,4 +116,22 @@ movements.forEach(function(mov, i, arr) {
     } else {
       console.log(`Movement ${i + 1}: you withdrew ${Math.abs(mov)}`)
   }
+})
+
+//MAP
+const currencies = new Map([
+  ['USD', 'United States dollar'],
+  ['EUR', 'Euro'],
+  ['GBP', 'Pound sterling'],
+]);
+
+currencies.forEach(function(value, key, map) {
+  console.log(`${key}: ${value}`);
+})
+
+//SET 
+const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR'])
+console.log(currenciesUnique)
+currenciesUnique.forEach(function(value, _, map) {
+  console.log(`${key}: ${value}`)
 })
