30ad04586ef227fd1323d86284bb814154d2b080
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-07-16 11:38:55 -0400

    array methods

--- 09-Data-Structures-Operators/final/script.js
+++ 09-Data-Structures-Operators/final/script.js
@@ -509,10 +509,33 @@ const game = {
   },
 };
 
+
+for (const [i, player] of game.scored.entries())
+console.log(`Goal ${i + 1}: ${player}`);
+
+const odds = Object.values(game.odds)
+let average = 0
+for (const odd of Object.values(game.odds))
+  average += odd
+  average /= odds.length  
+  console.log(average)
+
+
+for(const [team, odd] of Object.entries(game.odds)){
+const teamStr = team === "x" ? 'draw': `victory  ${game[team]}`
+console.log(`Odd of ${teamStr} ${odd}`)
+}
+
+const scorers = {}
+for(const player of game.scored){
+  scorers[player] ? scorers[player]++ : (scorers[player] = 1)
+  }
+  console.log(scorers)
 /*
 // 1.
 for (const [i, player] of game.scored.entries())
   console.log(`Goal ${i + 1}: ${player}`);
+
 
 // 2.
 const odds = Object.values(game.odds);
