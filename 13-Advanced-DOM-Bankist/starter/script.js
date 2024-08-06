a06e705a9748759c193a0b3c67b02d11a9fd9867
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-08-06 17:03:20 -0400

    slider part 1

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -160,20 +160,68 @@ allSections.forEach(function (section) {
 
 // Lazy loading images
 const imgTargets = document.querySelectorAll('img[data-src]');
-const loadImg = function( entries, observer) {
-  const [entry] = entries
-  if(!entry.isIntersecting) return;
+const loadImg = function (entries, observer) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) return;
   //replace src with data-src
   entry.target.src = entry.target.dataset.src;
 
-  entry.target.addEventListener('load', function() {
-    entry.target.classList.remove('lazy-img')
-  })
+  entry.target.addEventListener('load', function () {
+    entry.target.classList.remove('lazy-img');
+  });
   observer.unobserve(entry.target);
-}
+};
 const imgObserver = new IntersectionObserver(loadImg, {
   root: null,
-  threshold: 0
-})
+  threshold: 0,
+  rootMargin: '200px',
+});
 
-imgTargets.forEach(img => imgObserver.observe(img))
+imgTargets.forEach(img => imgObserver.observe(img));
+
+//slider
+const slides = document.querySelectorAll('.slide');
+const btnLeft = document.querySelector('.slider__btn--left');
+const btnRight = document.querySelector('.slider__btn--right');
+const dotContainer = document.querySelector('.dots')
+let curSlide = 0;
+const maxSlide = slides.length;
+
+slides.forEach((s, i) => (s.style.transform = `translateX(${100 * i})`));
+const slider = document.querySelector('.slider');
+slider.style.tranform = 'scale';
+
+const createDots = function() {
+  
+}
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+  );
+};
+goToSlide(0);
+// Next slide
+const nextSlide = function () {
+  if (curSlide === maxSlide - 1) {
+    curSlide = 0;
+  } else {
+    curSlide++;
+  }
+  goToSlide(curSlide);
+};
+//previous slide
+const prevSlide = function () {
+  if (curSlide === 0) {
+    curSlide = maxSlide - 1;
+  } else {
+    curSlide--;
+  }
+  goToSlide(curSlide);
+};
+btnRight.addEventListener('click', nextSlide);
+btnLeft.addEventListener('click', prevSlide);
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === 'ArrowLeft') prevSlide()
+  e.key === 'ArrowRight' && nextSlide()
+})
