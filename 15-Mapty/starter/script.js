9d2a87ef4facc6307d102e00a9df9c1bdd53de11
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-09-05 12:39:36 -0400

    geolocation

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -14,9 +14,22 @@ const inputElevation = document.querySelector('.form__input--elevation');
 if (navigator.geolocation)
   navigator.geolocation.getCurrentPosition(
     function (position) {
-      const {latitude} = position.coords
-      const {longitude} = position.coords
-      console.log(longitude, latitude)
+      const { latitude } = position.coords;
+      const { longitude } = position.coords;
+      console.log(longitude, latitude);
+      const coords = [latitude, longitude]
+      const map = L.map('map').setView(coords, 13);
+        
+        
+      L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Could not get your position.');
