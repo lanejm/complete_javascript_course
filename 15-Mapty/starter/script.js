4ed3881a83d4ee27f038d9fe3736b34ff64540b7
Joshua Lane <67156874+lanejm@users.noreply.github.com>
2024-09-05 13:22:19 -0400

    geoloaction popup

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -17,19 +17,31 @@ if (navigator.geolocation)
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(longitude, latitude);
-      const coords = [latitude, longitude]
+      const coords = [latitude, longitude];
       const map = L.map('map').setView(coords, 13);
-        
-        
+
       L.tileLayer('https://tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
         attribution:
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+      map.on('click', function (mapEvent) {
+        const { lat, lng } = mapEvent.latlng;
+        L.marker([lat, lng])
+          .addTo(map)
+          .bindPopup(
+            L.popup({
+                //change this information below to alter popup style/function
+              maxWidth: 250,
+              minWidth: 100,
+              autoClose: false,
+              closeOnClick: false,
+              className: 'running-popup',
+            })
+          )
+          .setPopupContent('found a cat here')
+          .openPopup();
+      });
     },
     function () {
       alert('Could not get your position.');
